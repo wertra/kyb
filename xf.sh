ebae570d7aa3caeca544179fa6b8c0ea3b4c47e5
wertra <103866834+wertra@users.noreply.github.com>
2022-09-12 18:01:28 -0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "1fe97f0f-7c52-4e84-bcd6-d2116cfc199e",
+                        "id": "d1410a78-3307-11ed-a261-0242ac120002",
                         "alterId": 0
                     }
                 ],
